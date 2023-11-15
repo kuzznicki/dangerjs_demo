e3dc9af92a0d0e5c0228a45ba6921dbd920e1a25
kuzznicki <kuznicki@protonmail.com>
2023-11-16 00:41:38 +0100

    update dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,6 @@
 import { message, danger, warn, fail } from "danger";
 
-const modifiedMD = danger.git.modified_files.join("\n - ");
+const modifiedMD = danger.git.modified_files.join("  \n - ");
 message("Changed Files in this PR: \n - " + modifiedMD);
 
 const commits = danger?.github?.commits || [];
@@ -8,9 +8,9 @@ const commits = danger?.github?.commits || [];
 if (commits.length) {
   const commitsMD = danger.github.commits
     .map((c) => c.commit.message)
-    .join("\n - ");
+    .join("  \n - ");
 
-  message(`Commits (${commits.length}): \n - ` + commitsMD);
+  message(`Commits (${commits.length}):  \n - ` + commitsMD);
 
   if (commits.length > 2) {
     warn("Try to reduce number of commits");
